c1cdf97c6710139412ee9b509dfd72ba74a5c3e1
“OnkarGavali” <onkargavali1561@gmail.com>
2021-10-21 21:01:10 +0530

    Extra code removed

--- LA2/script.js
+++ LA2/script.js
@@ -1,39 +1,35 @@
 function validate(){
     var name = document.getElementById("name").value;
-    var subject = document.getElementById("subject").value;
-    var phone = document.getElementById("phone").value;
     var email = document.getElementById("email").value;
     var message = document.getElementById("message").value;
     var error_message = document.getElementById("error_message");
-    
     error_message.style.padding = "10px";
-    
+    console.log(name);
+     console.log(email); 
+     console.log(message);
     var text;
     if(name.length < 5){
       text = "Please Enter valid Name";
       error_message.innerHTML = text;
-      return false;
-    }
-    if(subject.length < 10){
-      text = "Please Enter Correct Subject";
-      error_message.innerHTML = text;
-      return false;
-    }
-    if(isNaN(phone) || phone.length != 10){
-      text = "Please Enter valid Phone Number";
-      error_message.innerHTML = text;
+      alert("Form ");
+       console.log('1');
       return false;
     }
     if(email.indexOf("@") == -1 || email.length < 6){
       text = "Please Enter valid Email";
       error_message.innerHTML = text;
+      alert("Submitted");
+       console.log('3');
       return false;
     }
-    if(message.length <= 140){
-      text = "Please Enter More Than 140 Characters";
+    if(message.length <= 20){
+      text = "Please Enter More Than 20 Characters";
       error_message.innerHTML = text;
+      alert("Successfully!");
+       console.log('4');
       return false;
     }
     alert("Form Submitted Successfully!");
+     console.log('555');
     return true;
   }
